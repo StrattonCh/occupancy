7fd074b263550c76d02f60d6db652db84fc6f3e3
Christian Stratton <christianstratton@montana.edu>
2021-01-15 16:01:46 -0700

    updated occ_mod_ex.R
    added names(data)

--- examples/occ_mod_ex.R
+++ examples/occ_mod_ex.R
@@ -1,6 +1,7 @@
-# simulate data - note structure
+# simulate data - note structure of data
 sim <- sim_occ(M = 250, max_j = 10, seed = 11201995, rand_visits = FALSE)
 data <- sim$data
+names(data)
 
 # fit model
 ex <- occ_mod(occupancy = ~ psi_cov1, detection = ~ p_cov1, data = data,
