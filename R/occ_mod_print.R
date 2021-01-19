c68482b3b63dbd5f36fbdfd0e01b6c843412eb3c
Christian Stratton <christianstratton@montana.edu>
2021-01-18 19:00:05 -0700

    updated print.occ_mod
    Added code and mcmcinfo attributes

--- R/occ_mod_print.R
+++ R/occ_mod_print.R
@@ -7,5 +7,9 @@
 #' @export
 
 print.occ_mod <- function(x, ...) {
-  cat("This is just a dummy function.\n")
+  cat("Single species, single season site occupancy model fit using NIMBLE.\n")
+  cat("\nThe following model was fit:\n")
+  cat(attr(x, "code"))
+  cat("\n\nThe following MCMC algorithm was used:\n")
+  cat(paste0(attr(x, "mcmcinfo"), sep = "\n"))
 }
