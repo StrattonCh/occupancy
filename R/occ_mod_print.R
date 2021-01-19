5a58772c4ccb9467bebe2b6285d7624b2f7584a0
Christian Stratton <christianstratton@montana.edu>
2021-01-18 18:08:42 -0700

    added print method for occ_mod

---
+++ R/occ_mod_print.R
@@ -0,0 +1,11 @@
+#' Print method for "occ_mod" class
+#'
+#' @param x An object of class \link{occ_mod}
+#' @param ... Other arguments passed to or from other methods
+#'
+#' @export print.occ_mod
+#' @export
+
+print.occ_mod <- function(x, ...) {
+  cat("This is just a dummy function.\n")
+}
